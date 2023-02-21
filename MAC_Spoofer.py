a617f19996cd935b952dc580ee39bc31fa784e7e
Unknow <83958340+Ebooslilhacker@users.noreply.github.com>
2023-02-21 21:55:10 +0000

    Update MAC_Spoofer.py

--- MAC_Spoofer.py
+++ MAC_Spoofer.py
@@ -1,15 +1,8 @@
 import subprocess
-import optparse
 
 def change_mac(interface, new_mac):
     print("[+] Cambiando Dirección MAC para " + interface + " to " + new_mac)
-
-parser = optparse.OptionParser()
-
-parser.add_option("-i", "--interface", dest = "interface", help=" Interfaz para cambiar dirección MAC")
-parser.add_option("-m", "--mac", dest = "new_mac", help="Nueva dirección MAC")
-
-parser.parse_args()
+    
 
 interface = input("interface > ")
 new_mac = input("nuevo MAC > ") 
